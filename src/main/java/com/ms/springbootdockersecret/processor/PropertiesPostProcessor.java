6dfd004e6397f1714a1ce9de44cdbdaa4a30fbd2
muditshukla3 <muditshukla3@gmail.com>
2021-10-04 22:15:42 -0500

    Updated post processor

--- src/main/java/com/ms/springbootdockersecret/processor/PropertiesPostProcessor.java
+++ src/main/java/com/ms/springbootdockersecret/processor/PropertiesPostProcessor.java
@@ -2,11 +2,11 @@ package com.ms.springbootdockersecret.processor;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
+import java.util.List;
+import java.util.LinkedHashMap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
@@ -19,47 +19,34 @@ public class PropertiesPostProcessor implements EnvironmentPostProcessor {
 	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 		
 		String bindPathPpty = environment.getProperty("docker-secret.bind-path");
-		
-		System.out.println("value of \"docker-secret.bind-path\" property:" + bindPathPpty);
-		
+
+        Map<String,Object> dockerSecrets = new LinkedHashMap<>();
 		if (bindPathPpty!=null) {
-			Path bindPath = Paths.get(bindPathPpty);
-			if (Files.isDirectory(bindPath)) {
-				Map<String,Object> dockerSecrets;
-				try {
-					dockerSecrets = 
-					  Files.list(bindPath)
-					    .collect(
-					      Collectors.toMap(
-					        path -> {
-					        	File secretFile = path.toFile();
-					        	return "docker-secret-" + secretFile.getName();
-					        },
-					        path -> {
-					        	File secretFile = path.toFile();
-								try {
-									byte[] content = FileCopyUtils.copyToByteArray(secretFile);
-									return new String(content);
-								} catch (IOException e) {
-									throw new RuntimeException(e);
-								}
-					        }
-					      ));
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
+			File dockerFile = new File(bindPathPpty);
+			
+			if( dockerFile.exists()) {
+				Optional<File[]> files = Optional.ofNullable(dockerFile.listFiles());
 				
-				dockerSecrets
-				  .entrySet()
-				    .forEach(entry -> {
-				    	System.out.println(entry.getKey()+"=\""+entry.getValue()+"\"");
-				    });
-				
-				MapPropertySource pptySource = new MapPropertySource("docker-secrets",dockerSecrets);
-				
-				environment.getPropertySources().addLast(pptySource);
-				
+				files.ifPresent(item ->  {
+					List<File> itemArray = Arrays.asList(item);
+					itemArray.forEach(secretFile -> {
+						String key = "docker-secret-" + secretFile.getName();
+						byte[] content = null;
+						try {
+							content = FileCopyUtils.copyToByteArray(secretFile);
+							dockerSecrets.put(key, new String(content));
+						} catch (IOException e) {
+							System.err.println(e.getMessage());
+						}
+					});
+					
+				});
 			}
-		}			
+				
+			MapPropertySource pptySource = new MapPropertySource("docker-secrets",dockerSecrets);
+			
+			environment.getPropertySources().addLast(pptySource);
+				
+		}				
 	}
 }
